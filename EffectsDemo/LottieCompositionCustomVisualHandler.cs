7e41be35efc0956733de489eca19a5f293d16a22
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2023-09-03 20:43:06 +0200

    Update LottieCompositionCustomVisualHandler.cs

--- EffectsDemo/LottieCompositionCustomVisualHandler.cs
+++ EffectsDemo/LottieCompositionCustomVisualHandler.cs
@@ -111,33 +111,29 @@ internal class LottieCompositionCustomVisualHandler : CompositionCustomVisualHan
                   uniform float3 iImageResolution; // iImage1 resolution (pixels)
                   uniform shader iImage1;          // An input image.
                   
-                  // Source: @zozuar https://twitter.com/zozuar/status/1492217553103503363
+                  // Source: @kamoshika_vrc https://twitter.com/kamoshika_vrc/status/1495081980278751234
                   
-                  vec3 hsv(float h, float s, float v){
-                      vec4 t = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
-                      vec3 p = abs(fract(vec3(h) + t.xyz) * 6.0 - vec3(t.w));
-                      return v * mix(vec3(t.x), clamp(p - vec3(t.x), 0.0, 1.0), s);
+                  const float PI2 = 6.28318530718;
+                  float F(vec2 c){
+                    return fract(sin(dot(c, vec2(12.9898, 78.233))) * 43758.5453);
                   }
                   
-                  vec4 main(vec2 FC) {
-                    float e=0,R=0,t=iTime,s;
-                    vec2 r = iResolution.xy;
-                    vec3 q=vec3(0,0,-1), p, d=vec3((FC.xy-.5*r)/r.y,.7);
-                    vec4 o=vec4(0);
-                    for(float i=0;i<100;++i) {
-                      o.rgb+=hsv(.1,e*.4,e/1e2)+.005;
-                      p=q+=d*max(e,.02)*R*.3;
-                      float py = (p.x == 0 && p.y == 0) ? 1 : p.y;
-                      p=vec3(log(R=length(p))-t,e=asin(-p.z/R)-1.,atan(p.x,py)+t/3.);
-                      s=1;
-                      for(int z=1; z<=9; ++z) {
-                        e+=cos(dot(sin(p*s),cos(p.zxy*s)))/s;
-                        s+=s;
+                  half4 main(float2 FC) {
+                    vec4 o;
+                    float t = iTime;
+                    vec2 r = iResolution.xy * vec2(1, -1);
+                    vec3 R=normalize(vec3((FC.xy*2.-r)/r.y,1));
+                    for(float i=0; i<100; ++i) {
+                      float I=floor(t/.1)+i;
+                      float d=(I*.1-t)/R.z;
+                      vec2 p=d*R.xy+vec2(sin(t+F(I.xx)*PI2)*.3+F(I.xx*.9),t+F(I.xx*.8));
+                      if (F(I/100+ceil(p))<.2) {
+                        o+=smoothstep(.1,0.,length(fract(p)-.5))*exp(-d*d*.04);
                       }
-                      i>50.?d/=-d:d;
                     }
                     return o;
                   }
+                  
                   """;
 
         _time = 0f;

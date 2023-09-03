29b77ce6f17f99a82f3cf658d47eb5647a83f506
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2023-09-03 20:41:47 +0200

    Update LottieCompositionCustomVisualHandler.cs

--- EffectsDemo/LottieCompositionCustomVisualHandler.cs
+++ EffectsDemo/LottieCompositionCustomVisualHandler.cs
@@ -111,49 +111,32 @@ internal class LottieCompositionCustomVisualHandler : CompositionCustomVisualHan
                   uniform float3 iImageResolution; // iImage1 resolution (pixels)
                   uniform shader iImage1;          // An input image.
                   
-                  // Source: @zozuar https://twitter.com/zozuar/status/1482754721450446850
-                  mat2 rotate2D(float r){
-                      return mat2(cos(r), sin(r), -sin(r), cos(r));
+                  // Source: @zozuar https://twitter.com/zozuar/status/1492217553103503363
+                  
+                  vec3 hsv(float h, float s, float v){
+                      vec4 t = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+                      vec3 p = abs(fract(vec3(h) + t.xyz) * 6.0 - vec3(t.w));
+                      return v * mix(vec3(t.x), clamp(p - vec3(t.x), 0.0, 1.0), s);
                   }
                   
-                  mat3 rotate3D(float angle, vec3 axis){
-                      vec3 a = normalize(axis);
-                      float s = sin(angle);
-                      float c = cos(angle);
-                      float r = 1.0 - c;
-                      return mat3(
-                          a.x * a.x * r + c,
-                          a.y * a.x * r + a.z * s,
-                          a.z * a.x * r - a.y * s,
-                          a.x * a.y * r - a.z * s,
-                          a.y * a.y * r + c,
-                          a.z * a.y * r + a.x * s,
-                          a.x * a.z * r + a.y * s,
-                          a.y * a.z * r - a.x * s,
-                          a.z * a.z * r + c
-                      );
-                  }
-                  
-                  half4 main(float2 FC) {
-                    vec4 o = vec4(0);
+                  vec4 main(vec2 FC) {
+                    float e=0,R=0,t=iTime,s;
                     vec2 r = iResolution.xy;
-                    vec3 v = vec3(1,3,7), p = vec3(0);
-                    float t=iTime, n=0, e=0, g=0, k=t*.2;
-                    for (float i=0; i<100; ++i) {
-                      p = vec3((FC.xy-r*.5)/r.y*g,g)*rotate3D(k,cos(k+v));
-                      p.z += t;
-                      p = asin(sin(p)) - 3.;
-                      n = 0;
-                      for (float j=0; j<9.; ++j) {
-                        p.xz *= rotate2D(g/8.);
-                        p = abs(p);
-                        p = p.x<p.y ? n++, p.zxy : p.zyx;
-                        p += p-v;
+                    vec3 q=vec3(0,0,-1), p, d=vec3((FC.xy-.5*r)/r.y,.7);
+                    vec4 o=vec4(0);
+                    for(float i=0;i<100;++i) {
+                      o.rgb+=hsv(.1,e*.4,e/1e2)+.005;
+                      p=q+=d*max(e,.02)*R*.3;
+                      float py = (p.x == 0 && p.y == 0) ? 1 : p.y;
+                      p=vec3(log(R=length(p))-t,e=asin(-p.z/R)-1.,atan(p.x,py)+t/3.);
+                      s=1;
+                      for(int z=1; z<=9; ++z) {
+                        e+=cos(dot(sin(p*s),cos(p.zxy*s)))/s;
+                        s+=s;
                       }
-                      g += e = max(p.x,p.z) / 1e3 - .01;
-                      o.rgb += .1/exp(cos(v*g*.1+n)+3.+1e4*e);
+                      i>50.?d/=-d:d;
                     }
-                    return o.xyz1;
+                    return o;
                   }
                   """;
 
